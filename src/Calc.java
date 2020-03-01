a1ebcb13e058b37a546b1687250158a03eccfc67
ibrahim <msri.ibraheem@gmail.com>
2020-03-02 00:01:55 +0200

    first commit

---
+++ src/Calc.java
@@ -0,0 +1,9 @@
+
+public class Calc {
+
+	public int addNums(int a, int b) {
+		
+		return a+b;
+	}
+
+}
---
+++ test/CalcTest.java
@@ -0,0 +1,22 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CalcTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void testAdd_twoPoisitveInputs_positiveOutput() {
+		int a = 5;
+		int b = 6;
+		Calc c= new Calc();
+		int d = c.addNums(a,b);
+		Assert.assertTrue(d==11);
+	}
+
+}
