713d3a0d1021904625596ed809bb87d87caa0c19
ibrahim <msri.ibraheem@gmail.com>
2020-03-02 18:51:44 +0200

    added build.xml with extra libraries

--- src/Calc.java
+++ src/Calc.java
@@ -1,9 +1,16 @@
 
 public class Calc {
 
-	public int addNums(int a, int b) {
+public int addNums(int a, int b) {
 		
 		return a+b;
 	}
+	public static void main(String[] args) {
+		System.out.println("Built");
+
+	}
 
 }
+
+
+
--- test/CalcTest.java
+++ test/CalcTest.java
@@ -1,8 +1,11 @@
+
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.Assert;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+
 
 class CalcTest {
 
